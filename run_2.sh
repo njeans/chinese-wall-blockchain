f1bba667b9de85d3c42b771556180068d69b3dbc
njeans <nerlajeanlouis@gmail.com>
2021-04-18 13:38:25 -0500

    script

---
+++ run_2.sh
@@ -0,0 +1,13 @@
+set -e
+PROJECT_ROOT=$(pwd)
+cd "$PROJECT_ROOT/deploy/keygen"
+go build
+./keygen 2 "$PROJECT_ROOT/deploy/2-org-1-orderer/keys"
+cd "$PROJECT_ROOT/deploy/2-org-1-orderer"
+echo "" > "$PROJECT_ROOT/log/2_node_eval.log"
+./restartNetwork.sh 2>&1 | tee -a $PROJECT_ROOT/log/2_node_eval.log
+
+cd "$PROJECT_ROOT/app"
+npm install
+export NUM_ORGS=2
+./run.sh 2>&1 | tee -a "$PROJECT_ROOT/log/2_node_eval.log"
