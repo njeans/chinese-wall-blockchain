6c0b0c8cc28ed8e9e4969493f7dde6feaa6ac055
njeans <nerlajeanlouis@gmail.com>
2021-04-18 13:47:18 -0500

    update script

--- run_2.sh
+++ run_2.sh
@@ -2,10 +2,10 @@ set -e
 PROJECT_ROOT=$(pwd)
 cd "$PROJECT_ROOT/deploy/keygen"
 go build
-mkdir "$PROJECT_ROOT/deploy/2-org-1-orderer/keys"
+mkdir -p "$PROJECT_ROOT/deploy/2-org-1-orderer/keys"
 ./keygen 2 "$PROJECT_ROOT/deploy/2-org-1-orderer/keys"
 cd "$PROJECT_ROOT/deploy/2-org-1-orderer"
-mkdir "$PROJECT_ROOT/log/"
+mkdir -p "$PROJECT_ROOT/log/"
 echo "" > "$PROJECT_ROOT/log/2_node_eval.log"
 ./restartNetwork.sh 2>&1 | tee -a $PROJECT_ROOT/log/2_node_eval.log
 
